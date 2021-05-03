4dffc4077eea0ba8a96286f20df9979eea3e466e
Cha DongHyeon <chadonghyeon@kakao.com>
2021-05-03 22:58:54 +0900

    20210503
    rev.-1

--- AccountBook/main.c
+++ AccountBook/main.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
-#include <sqlite3.h> // in "AccountBook" folder
+#include <stdio.h> // In Windows 10
+#include <stdlib.h>
+#include "sqlite3.h" // in Project folder
 
 int save();
 int UI();
@@ -7,31 +8,49 @@ int UI();
 int main(void)
 {
 int num;
-printf("실행할 작업의 번호를 입력해주세요.");
-scanf("%d",&num);
-system("cls");
 UI(num);
 sqlite3 *db; // handle of database
 char *err_msg=0; // if occur errors, this pointer point to error msg
 
+if(num==0) system("exit");
+
 if(num==1) // no.1 is input information in database file
 {
-int rc=sqlite3_open("date.db",&db); // open "date.db" database file
-if(rc!=SQLITE_OK)
-{
-    fprintf(stderr,"Cannot Open Database File (file category : %s\n",sqlite3_errmsg(db));
-    sqlite3_close(db);
-    return 1;
-}
-char *sql="DROP TABLE IF EXISTS Info;"
-"CREATE TABLE Info(Date INT,List TEXT, Price INT);"
+    int rc=sqlite3_open("date.db",&db); // open "date.db" database file
+    if(rc!=SQLITE_OK)
+    {
+        fprintf(stderr,"Cannot Open Database File ( %s ) \n",sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return 1;
+    }
+    char *sql="DROP TABLE IF EXISTS Info;"
+            "CREATE TABLE Info(Date INT,List TEXT, Price INT);"
+            "INSERT INTO Info(20210503,'Test',100000;";
+    rc=sqlite3_exec(db,sql,0,0,&err_msg);
+    if(rc!=SQLITE_OK) //occur error when close database file
+    {
+        fprintf(stderr,"SQL Error ( %s ) \n",err_msg);
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+        return 1;
+    }
+    sqlite3_close(db); // close database file
+    return 0;
 }
 
-return 0;
+if (num==2)
+{
+
+}
+
 }
 
 int UI(int num)
 {
-
+printf("1. 정보 입력\n");
+printf("2. 정보 조회\n");
+printf("0. 프로그램 종료\n");
+printf("실행할 작업의 숫자를 입력해주세요 : ");
+scanf("%d",&num);
 }
 //system("command") -> typing "command" in terminal
