c577d6cba7fce5edb993b4b435ebac22446bafab
Cha DongHyeon <chadonghyeon@kakao.com>
2021-05-05 15:37:14 +0900

    210505
    Account Book
    rev -1.0.1

--- AccountBook/main.c
+++ AccountBook/main.c
@@ -1,12 +1,16 @@
-//"gcc -o accountbook sqlite3.c main.c" is build command 
-#include <stdio.h> // In Windows 10
+// "gcc -o accountbook sqlite3.c main.c" is build command 
+#include <stdio.h> // standard input/output header
 #include <stdlib.h> // To Use system function
-#include "sqlite3.h" // in Project folder
+#include "sqlite3.h" // To Use sqlite3
 
-
-int save(); int modi(); int load();
+int save(); int modi(); int load(); 
+int insert();
 int UI(); int reset_nNum();
-
+struct {
+int Date;
+char List[100];
+int Price;
+} d1;
 
 /***********************Below this line is a main function***********************/
 
@@ -43,6 +47,7 @@ nNum=0;
 
 int save()
 {
+int nNum_value=0;
 sqlite3 *db; // handle of database
 char *err_msg=0; // if occur errors, this pointer point to error msg
 int rc=sqlite3_open("date.db",&db); // open "date.db" database file
@@ -53,8 +58,11 @@ int rc=sqlite3_open("date.db",&db); // open "date.db" database file
         return 1;
     }
     char *sql="DROP TABLE IF EXISTS Info;"
-              "CREATE TABLE Info(Date INT,List TEXT, Price INT);"
-              "INSERT INTO Info(20210503,'Test',100000;";
+              "CREATE TABLE Info(Date INT,List TEXT, Price INT);";
+              while(nNum_value=0)
+              {
+              "INSERT INTO Info(d1.Date,d1.List,d1.Price);";
+              }
     rc=sqlite3_exec(db,sql,0,0,&err_msg);
     if(rc!=SQLITE_OK) //occur error when close database file
     {
@@ -66,6 +74,19 @@ int rc=sqlite3_open("date.db",&db); // open "date.db" database file
     sqlite3_close(db); // close database file
     printf("정보가 입력되었습니다.");
     return 0;
+}
+
+int insert(int nNum_value)
+{
+   int nNum_value=0;
+   printf("몇 개의 데이터를 삽입하시겠습니까? : ");
+   scanf("%d",&nNum_value);
+   while(nNum_value=0)
+   {
+   printf("\n어떤 데이터를 삽입하시겠습니까? 날짜(숫자로만 이루어져 있어야 함),내용,가격 순으로 입력해주세요. 현재 %d 번째 데이터를 삽입중입니다.",nNum_value);
+   scanf("%d,%c,%d",&d1.Date,d1.List,&d1.Price);
+   nNum_value=nNum_value-1;
+   }
 }
 
 int modi()
